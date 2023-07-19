c15248d03fede94c66a51664785bff1f02f52d1f
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2023-07-19 21:11:59 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,11 +35,62 @@ class PasswordGenerator {
     }
 };
 
-// Rest of AdvancedPasswordGenerator class
+class AdvancedPasswordGenerator : public PasswordGenerator
+{
+public:
+    string generatePassword(int length, bool includeNumbersAndSymbols) override
+    {
+        string password = PasswordGenerator::generatePassword(length, includeNumbersAndSymbols);
 
-int main() {
+        // Add uppercase letters to the password
+        for (int i = 0; i < password.size(); i++)
+        {
+            if (rand() % 2 == 0)
+            {
+                password[i] = toupper(password[i]);
+            }
+        }
 
-  // Rest of main function 
-  
-  return 0;
+        return password;
+    }
+};
+int main()
+{
+    PasswordGenerator *generator = new AdvancedPasswordGenerator();
+    vector<string> passwords;
+
+    while (true)
+    {
+        // Take password length from user
+        cout << "Enter the length of the password (0 to exit): ";
+        int length;
+        cin >> length;
+
+        if (length == 0)
+        {
+            break;
+        }
+
+        // Prompt user to include numbers and symbols
+        cout << "Include numbers and symbols in the password (y/n)? ";
+        char input;
+        cin >> input;
+        bool includeNumbersAndSymbols = (input == 'y');
+
+        // Generate password
+        string password = generator->generatePassword(length, includeNumbersAndSymbols);
+
+        // Display password
+        cout << "Your password is: " << password << endl;
+
+        passwords.push_back(password);
+    }
+
+    // Display list of generated passwords
+    cout << "Generated passwords:" << endl;
+    for (int i = 0; i < passwords.size(); i++)
+    {
+        cout << passwords[i] << endl;
+    }
+    return 0;
 }
